d6e645d4146aa594c0843384d1095542057a74f2
Dvine <dvine.bb@gmail.com>
2018-04-20 17:47:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+APIEvents
+=========
+
+A Symfony project created on April 20, 2018, 3:00 pm.
---
+++ src/AppBundle/Entity/Event.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Event
+ *
+ * @ORM\Table(name="event")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EventRepository")
+ */
+class Event
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="referrer", type="string", length=255)
+     */
+    private $referrer;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="cookie_id", type="integer", unique=true)
+     */
+    private $cookieId;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+
+    /**
+     * Event constructor.
+     * @param \DateTime $createdAt
+     */
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Event
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set referrer
+     *
+     * @param string $referrer
+     *
+     * @return Event
+     */
+    public function setReferrer($referrer)
+    {
+        $this->referrer = $referrer;
+
+        return $this;
+    }
+
+    /**
+     * Get referrer
+     *
+     * @return string
+     */
+    public function getReferrer()
+    {
+        return $this->referrer;
+    }
+
+    /**
+     * Set cookieId
+     *
+     * @param integer $cookieId
+     *
+     * @return Event
+     */
+    public function setCookieId($cookieId)
+    {
+        $this->cookieId = $cookieId;
+
+        return $this;
+    }
+
+    /**
+     * Get cookieId
+     *
+     * @return int
+     */
+    public function getCookieId()
+    {
+        return $this->cookieId;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Event
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
+
